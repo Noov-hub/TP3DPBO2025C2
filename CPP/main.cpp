fe0e3ce2c8e08a3b4c1a7c0fa5f0e8016a47a69a
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-03-16 01:48:23 +0700

    modified:   CPP/main.cpp

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -1,29 +1,71 @@
 #include <iostream>
-#include <string>
-#include "Classnya.cpp"
-
+#include <vector>
 using namespace std;
 
+// Kelas Komponen sebagai superclass
+class Komponen {
+protected:
+    string Nama;
+    string Merk;
+public:
+    Komponen(string nama, string merk) : Nama(nama), Merk(merk) {}
+    virtual void tampilkanInfo() {
+        cout << "Nama: " << Nama << ", Merk: " << Merk << endl;
+    }
+};
 
+// Kelas Overclockable untuk multiple inheritance
+class Overclockable {
+protected:
+    float OverclockLimit;
+public:
+    Overclockable(float limit) : OverclockLimit(limit) {}
+    void tampilkanOverclockInfo() {
+        cout << "Overclock Limit: " << OverclockLimit << " GHz" << endl;
+    }
+};
+
+// Kelas Processor mewarisi Komponen dan Overclockable
+class Processor : public Komponen, public Overclockable {
+private:
+    float ClockSpeed;
+    float MaxTDP;
+public:
+    Processor(string nama, string merk, float clock, float tdp, float limit)
+        : Komponen(nama, merk), Overclockable(limit), ClockSpeed(clock), MaxTDP(tdp) {}
+
+    void tampilkanInfo() override {
+        Komponen::tampilkanInfo();
+        cout << "Clock Speed: " << ClockSpeed << " GHz, Max TDP: " << MaxTDP << "W" << endl;
+        tampilkanOverclockInfo();
+    }
+};
+
+// Kelas Graphic mewarisi Komponen dan Overclockable
+class Graphic : public Komponen, public Overclockable {
+private:
+    float VramGB;
+    float MaxTDP;
+public:
+    Graphic(string nama, string merk, float vram, float tdp, float limit)
+        : Komponen(nama, merk), Overclockable(limit), VramGB(vram), MaxTDP(tdp) {}
+
+    void tampilkanInfo() override {
+        Komponen::tampilkanInfo();
+        cout << "VRAM: " << VramGB << " GB, Max TDP: " << MaxTDP << "W" << endl;
+        tampilkanOverclockInfo();
+    }
+};
+
+// Main Program
 int main() {
-    int n;
-    cout << "Welcome To Noov Petshop\n";
-    cout << "Infokan berapa Data yang ingin dimasukkan banh: "; cin >> n;
-    Classnya arraynya[n];
-    int pilihan, data = 0;
+    Processor cpu("Intel i9", "Intel", 3.5, 125, 5.2);
+    Graphic gpu("RTX 4080", "NVIDIA", 16, 320, 2.8);
 
-    do {
-        cout << "1 | Show\n2 | Add\n3 | Change\n4 | Delete\n5 | Search\n0 | Exit\n";
-        cout << "Pilih Menu: "; cin >> pilihan;
-        // switch (pilihan) {
-        //     case 1: showData(DataPetshop, n); break;
-        //     case 2: addData(DataPetshop, data, n); break;
-        //     case 3: changeData(DataPetshop, data); break;
-        //     case 4: deleteData(DataPetshop, data); break;
-        //     case 5: searchData(DataPetshop, data); break;
-        //     case 0: cout << "Thank You!\n"; break;
-        //     default: cout << "Pilihan tidak valid!\n";
-        // }
-    } while (pilihan != 0);
+    cout << "=== Info Processor ===" << endl;
+    cpu.tampilkanInfo();
+    cout << "\n=== Info GPU ===" << endl;
+    gpu.tampilkanInfo();
+
     return 0;
 }
